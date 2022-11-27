193222505b7164700034bd59a21b6c188984e350
CYD <374766849@qq.com>
2022-11-27 11:46:27 +0800

    Change detection condition (#5)
    Change detection condition

--- jksb_sysu.py
+++ jksb_sysu.py
@@ -87,16 +87,15 @@ def jksb(driver):
     logging.info("点击下一步")
     driver.find_element_by_xpath('//*[@id="form_command_bar"]/li[1]').click()
 
-    wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//*[@id='form_command_bar']/li[2]")) ) # 出现终止按钮
-    
-    logging.info("出现终止按钮，先睡一会")
-    time.sleep(5)
-    logging.info("醒了，看看下一步能不能点了")
-    wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//*[@id='form_command_bar']/li[1]")) ) # 等下一步能点
-    logging.info("可以咯，提交健康申报")
-    
-    driver.find_element_by_xpath('//*[@id="form_command_bar"]/li[1]').click()
 
+    wait.until(expected_conditions.text_to_be_present_in_element((By.XPATH, "//*[@id='form_command_bar']/li[1]"), '提交'))
+    logging.info("提交健康申报")
+    try:
+        wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//*[@id='form_command_bar']/li[1]")) ).click()
+    except Exception as e:
+        print(e)
+        raise e
+        
     result=""
     try:
         wait.until(expected_conditions.text_to_be_present_in_element
